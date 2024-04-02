68ec1bfde2a0415acb2e7698edcc2786ab7a0ba2
Trusha Shah <trushashah1402@gmail.com>
2024-04-02 19:40:32 +0530

    Complete the ListingItem Component

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -148,7 +148,7 @@ export default function Profile() {
         {!loading && listings.length > 0 && (
           <>
             <h2 className="text-2xl text-center font-semibold">My Listings</h2>
-            <ul>
+            <ul className="sm:grid grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 2xl:grid-cols-5">
               {listings.map((listing) => (
                 <ListingItem key={listing.id} id={listing.id} listing={listing.data} />
               ))}
