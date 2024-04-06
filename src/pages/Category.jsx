41ae14915cce6de03bd5ff3147823c9442a026e2
Trusha Shah <trushashah1402@gmail.com>
2024-04-06 17:13:22 +0530

    Create and complete the category page for rent and sale

--- src/pages/Category.jsx
+++ src/pages/Category.jsx
@@ -12,19 +12,21 @@ import {
 import { db } from "../firebase";
 import Spinner from "../components/Spinner";
 import ListingItem from "../components/ListingItem";
+import { useParams } from "react-router";
 
 
-export default function Offers() {
+export default function Category() {
   const [listings, setListings] = useState(null);
   const [loading, setLoading] = useState(true);
   const [lastFetchedListing, setLastFetchListing] = useState(null);
+  const params = useParams()
   useEffect(() => {
     async function fetchListings() {
       try {
         const listingRef = collection(db, "listings");
         const q = query(
           listingRef,
-          where("offer", "==", true),
+          where("type", "==", params.categoryName),
           orderBy("timestamp", "desc"),
           limit(4)
         );
@@ -46,14 +48,14 @@ export default function Offers() {
     }
 
     fetchListings();
-  }, []);
+  }, [params.categoryName]);
 
   async function onFetchMoreListings() {
     try {
       const listingRef = collection(db, "listings");
       const q = query(
         listingRef,
-        where("offer", "==", true),
+        where("type", "==", params.categoryName),
         orderBy("timestamp", "desc"),
         startAfter(lastFetchedListing),
         limit(4)
@@ -77,7 +79,7 @@ export default function Offers() {
 
   return (
     <div className="max-w-6xl mx-auto px-3">
-      <h1 className="text-3xl text-center mt-6 font-bold mb-6">Offers</h1>
+      <h1 className="text-3xl text-center mt-6 font-bold mb-6">{params.categoryName==="rent"?"Places for Rent" : "Places for Sale"}</h1>
       {loading ? (
         <Spinner />
       ) : listings && listings.length > 0 ? (
@@ -105,7 +107,7 @@ export default function Offers() {
           )}
         </>
       ) : (
-        <p>There are no current offers</p>
+        <p>There are no current {params.categoryName==="rent"?"places for Rent" : "places for Sale"}</p>
       )}
     </div>
   );
