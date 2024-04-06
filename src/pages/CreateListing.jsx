330fb2574754ab139f2a63d60d4e947e1ea68c6c
Trusha Shah <trushashah1402@gmail.com>
2024-04-06 16:04:21 +0530

    Create slider component and add it to home page and complete home page by adding recent offers, places for rent and places for sale

--- src/pages/CreateListing.jsx
+++ src/pages/CreateListing.jsx
@@ -215,7 +215,7 @@ export default function CreateListing() {
           value={name}
           onChange={onChange}
           placeholder="Name"
-          maxLength="32"
+          maxLength="100"
           minLength="10"
           required
           className="w-full px-4 py-2 text-xl text-gray-700 bg-white border border-gray-300 rounded transition duration-150 ease-in-out focus:text-gray-700 focus:bg-white focus:border-slate-600 mb-6"
@@ -347,7 +347,7 @@ export default function CreateListing() {
           value={description}
           onChange={onChange}
           placeholder="Description"
-          maxLength="32"
+          maxLength="400"
           minLength="10"
           required
           className="w-full px-4 py-2 text-xl text-gray-700 bg-white border border-gray-300 rounded transition duration-150 ease-in-out focus:text-gray-700 focus:bg-white focus:border-slate-600 mb-6"
